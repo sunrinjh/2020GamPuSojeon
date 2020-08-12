729e53b19d0817283583771187cce39daaa27405
sunrinjh <acer@4077.zz.am>
2020-08-12 16:21:28 +0900

    Day 3 Game Engine

---
+++ 3일차/xngin/xngin/SceneManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Scene.h"
+class SceneManager
+{
+private:
+	Scene* currentScene;
+public:
+	SceneManager();
+	~SceneManager();
+	void Render();
+	void Update(float dTime);
+
+	void ChangeScene(Scene* scene);
+};
+

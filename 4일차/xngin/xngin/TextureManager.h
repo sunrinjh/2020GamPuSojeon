86e8b15463428aed63fa329f0e2f415ada510a90
sunrinjh <acer@4077.zz.am>
2020-08-13 13:21:26 +0900

    지이이이건

---
+++ 4일차/xngin/xngin/TextureManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <d3dx9.h>
+#include <map>
+class TextureManager
+{
+private:
+	std::map<char*, LPDIRECT3DTEXTURE9> textureMap;
+public:
+	TextureManager();
+	~TextureManager();
+	LPDIRECT3DTEXTURE9 LoadTextureFromFile(char* path);
+
+};
+
